7661ea8662aa549b322ec6c37d6f0e370ac4e505
jamebluntcc <291552579@qq.com>
2018-01-24 21:10:38 +0800

    init

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jamebluntcc/myStudy/web_develop_learn/django1.8.3/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
