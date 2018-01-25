d9aa7a235b364168098f3eee8a52f195d3d6e6df
jamebluntcc <291552579@qq.com>
2018-01-25 11:32:42 +0800

    add sitebuilder

--- prototypes.py
+++ prototypes.py
@@ -1,27 +1,33 @@
+import os
 import sys
+
 
 from django.conf import settings
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 settings.configure(
-    DEBUG=True,
+    DEBUG=False,
     SECRET_KEY='hard to guess',
     ROOT_URLCONF='sitebuilder.urls',
+    ALLOWED_HOSTS=['0.0.0.0'],
     MIDDLEWARE_CLASSES=(),
-    INSTALL_APPS=(
+    INSTALLED_APPS=(
         'django.contrib.staticfiles',
         'sitebuilder',
     ),
-    TEMPLATE=(
+    TEMPLATES=(
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIR': [],
+            'DIRS': [os.path.join(BASE_DIR, 'sitebuilder', 'template'),],
             'APP_DIRS': True,
         },
     ),
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
     STATIC_URL='/static/',
 )
 
 if __name__  == '__main__':
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
+
     
